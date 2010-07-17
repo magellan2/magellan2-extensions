5314794311399a88fe126a5b0d1f2ad5f0ae3a9b
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2010-07-17 23:57:25 +0000

    added profile chooser

--- src/magellan/ext/console/merge/ConsoleMerger.java
+++ src/magellan/ext/console/merge/ConsoleMerger.java
@@ -3,6 +3,7 @@ package magellan.ext.console.merge;
 import java.io.File;
 import java.util.zip.ZipEntry;
 
+import magellan.client.utils.MagellanFinder;
 import magellan.library.GameData;
 import magellan.library.io.GameDataReader;
 import magellan.library.io.cr.CRWriter;
@@ -15,19 +16,20 @@ import magellan.library.utils.ReportMerger;
 import magellan.library.utils.Resources;
 
 /**
- * This little program loads a CR report, merges another CR report
- * and saves the merges report to a third file.
+ * This little program loads a CR report, merges another CR report and saves the
+ * merges report to a third file.
  */
 public class ConsoleMerger implements ReportMerger.Loader, ReportMerger.AssignData {
   private static File settingsDir = null;
+  private static File resourceDir = null;
 
   private File baseReport = null;
   private File mergeReport = null;
   private File resultReport = null;
-  
+
   private GameData base = null;
   private GameData merged = null;
-  
+
   /**
    *
    */
@@ -35,52 +37,52 @@ public class ConsoleMerger implements ReportMerger.Loader, ReportMerger.AssignDa
     this.baseReport = baseReport;
     this.mergeReport = mergeReport;
     this.resultReport = resultReport;
-    
+
     PropertiesHelper.setSettingsDirectory(settingsDir);
-    Resources.getInstance().initialize(settingsDir, "");
+    Resources.getInstance().initialize(resourceDir, "");
   }
-  
+
   /**
    *
    */
   protected void check() {
     if (!baseReport.exists() || !baseReport.canRead() || !baseReport.isFile()) {
-      System.out.println("base_report "+baseReport+" does not exist or isn't readable");
+      System.out.println("base_report " + baseReport + " does not exist or isn't readable");
       System.exit(1);
     }
-    
+
     if (!mergeReport.exists() || !mergeReport.canRead() || !mergeReport.isFile()) {
-      System.out.println("merge_report "+mergeReport+" does not exist or isn't readable");
+      System.out.println("merge_report " + mergeReport + " does not exist or isn't readable");
       System.exit(1);
     }
   }
-  
+
   /**
    *
    */
   public void run() {
-    System.out.println("Load Base Report: "+baseReport);
+    System.out.println("Load Base Report: " + baseReport);
     base = load(baseReport);
     if (base == null) {
       System.exit(2);
       return;
     }
-    System.out.println("Loaded succesfully with encoding: "+base.getEncoding());
-    
-    System.out.println("Merge Report: "+mergeReport);
+    System.out.println("Loaded succesfully with encoding: " + base.getEncoding());
+
+    System.out.println("Merge Report: " + mergeReport);
     ReportMerger merger = new ReportMerger(base, mergeReport, this, this);
     merged = merger.merge();
-    
+
     if (merged == null) {
       System.exit(2);
       return;
     }
-    
+
     try {
-      System.out.println("Save Report to "+resultReport);
+      System.out.println("Save Report to " + resultReport);
       FileType type = FileTypeFactory.singleton().createFileType(resultReport, false);
       NullUserInterface ui = new NullUserInterface();
-      CRWriter crw = new CRWriter(merged,ui,type,merged.getEncoding());
+      CRWriter crw = new CRWriter(merged, ui, type, merged.getEncoding());
       crw.writeSynchronously();
       crw.close();
     } catch (Exception exception) {
@@ -89,45 +91,52 @@ public class ConsoleMerger implements ReportMerger.Loader, ReportMerger.AssignDa
     }
   }
 
-
   /**
-   * Syntax: java magellan.ext.console.merge.ConsoleMerger <magellan_dir> <original_report> <merge_report> <result_report>
+   * Syntax: java magellan.ext.console.merge.ConsoleMerger <magellan_dir>
+   * <original_report> <merge_report> <result_report>
    */
   public static void main(String[] args) {
     System.setErr(System.out);
-    
+
     System.out.println("ConsoleMerger V.1.0.1");
     System.out.println("Author: Thoralf Rickert <thoralf@m84.de>");
     System.out.println("");
-    
-//    args = new String[4];
-//    args[0] = "D:/Eclipse Workspace/Magellan2";
-//    args[1] = "D:/TEMP/519-x6j3.zip";
-//    args[2] = "D:/TEMP/520-x6j3.zip";
-//    args[3] = "D:/TEMP/report.cr.bz2";
-    
+
+    // args = new String[4];
+    // args[0] = "D:/Eclipse Workspace/Magellan2";
+    // args[1] = "D:/TEMP/519-x6j3.zip";
+    // args[2] = "D:/TEMP/520-x6j3.zip";
+    // args[3] = "D:/TEMP/report.cr.bz2";
+
     if (args.length != 4) {
       System.out.println("Syntax:");
-      System.out.println("java magellan.ext.console.merge.ConsoleMerger <magellan_dir> <base_report> <merge_report> <result_report>");
-      System.out.println("  magellan_dir  - the directory that contains the magellan settings (rules and resources).");
+      System.out
+          .println("java magellan.ext.console.merge.ConsoleMerger <magellan_dir> <base_report> <merge_report> <result_report>");
+      System.out
+          .println("  magellan_dir  - the directory that contains the magellan settings (rules and resources).");
       System.out.println("  base_report   - the original report.");
       System.out.println("  merge_report  - the report that should be merged to <base_report>");
-      System.out.println("  result_report - the destination file. If the file exists, it'll be overwritten.");
+      System.out
+          .println("  result_report - the destination file. If the file exists, it'll be overwritten.");
       System.out.println("This program is aware of the file name extension and loads and saves");
       System.out.println("the file in the given format.");
       System.exit(1);
     }
-    
-    settingsDir = new File(args[0]);
-    if (!settingsDir.isDirectory()) {
+
+    resourceDir = new File(args[0]);
+
+    if (!resourceDir.isDirectory()) {
       System.out.println("<magellan_dir> must be a directory.");
       System.exit(1);
     }
-    
-    ConsoleMerger merger = new ConsoleMerger(new File(args[1]),new File(args[2]),new File(args[3]));
+
+    settingsDir = MagellanFinder.findSettingsDirectory(resourceDir, null);
+
+    ConsoleMerger merger =
+        new ConsoleMerger(new File(args[1]), new File(args[2]), new File(args[3]));
     merger.check();
     merger.run();
-    
+
     System.out.println("");
     System.out.println("OK");
   }
@@ -140,19 +149,22 @@ public class ConsoleMerger implements ReportMerger.Loader, ReportMerger.AssignDa
     NullUserInterface ui = new NullUserInterface();
 
     try {
-      data = new GameDataReader(ui).readGameData(FileTypeFactory.singleton().createFileType(file, true, new NullFileTypeChooser()));
+      data =
+          new GameDataReader(ui).readGameData(FileTypeFactory.singleton().createFileType(file,
+              true, new NullFileTypeChooser()));
     } catch (FileTypeFactory.NoValidEntryException e) {
-      System.out.println("Base Report contains no or multiple cr reports and Merger cannot choose the correct file.");
+      System.out
+          .println("Base Report contains no or multiple cr reports and Merger cannot choose the correct file.");
       System.exit(1);
     } catch (Exception exception) {
       exception.printStackTrace(System.out);
       System.exit(1);
     }
 
-    if (data!=null && data.outOfMemory) {
+    if (data != null && data.outOfMemory) {
       System.out.println("Running out of memory...take care!");
     }
-    
+
     return data;
   }
 
@@ -160,7 +172,7 @@ public class ConsoleMerger implements ReportMerger.Loader, ReportMerger.AssignDa
    * 
    */
   public void assign(GameData data) {
-    this.base = data;
+    base = data;
   }
 
 }
@@ -173,7 +185,8 @@ class NullFileTypeChooser extends FileTypeChooser {
   public ZipEntry chooseZipEntry(ZipEntry[] entries) {
     for (ZipEntry entry : entries) {
       String name = entry.getName();
-      if (name.toLowerCase().endsWith(".cr")) return entry;
+      if (name.toLowerCase().endsWith(".cr"))
+        return entry;
     }
     return null;
   }
